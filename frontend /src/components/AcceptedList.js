136880d1195af19a1e91965c099f2a24335569b7
Amelia01231 <amelia01@student.bbk.ac.uk>
2025-05-15 18:07:47 +0100

    frontend updated

--- frontend /src/components/AcceptedList.js
+++ frontend /src/components/AcceptedList.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfdda33a0f90df306f7735e51f3c091cb0250f6eb91271277a0e0b20f085ffe1
-size 672
+oid sha256:bbd6e05e531620e6229e47d2170f0a81ab7515b92c886317b32ba5f299184754
+size 4300
--- frontend /src/components/Main.js
+++ frontend /src/components/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75fe30d24f81446b91e309d42999b9387d491023ab3ae0d70b9c13f855505d44
-size 5209
+oid sha256:2d0512b8e3f4fcf20aac7bf6b264bb4a595ccb31cee81bd595ee6c54a65bf759
+size 5398
